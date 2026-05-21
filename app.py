e51ee7e1a539ae6b473b1126e8cf744824a2d5cd
Camilla Campbell <camilla.campbell@helsinki.fi>
2026-05-21 15:34:02 +0300

    Create form for creating products

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_product")
+def new_product():
+    return render_template("new_product.html")
+
+@app.route("/create_product", methods=["POST"])
+def create_product():
+    name = request.form["name"]
+    price = request.form["price"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO products (name, price, user_id) VALUES (?, ?, ?)"
+    db.execute(sql, [name, price, user_id])
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -42,10 +56,14 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql,[username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]  
+
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -54,4 +72,5 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    price INTEGER,
+    user_id INTEGER REFERENCES users
+);

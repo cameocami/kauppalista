911dd49abe6b5c50b337614cdb342bd8383b0c37
Camilla Campbell <camilla.campbell@helsinki.fi>
2026-06-07 17:08:47 +0300

    Refactor user creation and login handling

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import generate_password_hash, check_password_hash
-import db
 import config
 import products, users, units, departments
 import re
@@ -125,43 +122,32 @@ def show_user(user_id):
 def register():
     return render_template("register.html")
 
-@app.route("/create", methods=["POST"])
-def create():
+@app.route("/create_user", methods=["POST"])
+def create_user():
     username = request.form["username"]
     password1 = request.form["password1"]
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
-
-    try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
-    except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
-
+    if not users.check_availability(username):
+        return "VIRHE: käyttäjätunnus on jo käytössä"
+    users.create_user(username,password1)
     return "Tunnus luotu"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        result = db.query(sql,[username])[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
-        
-        if check_password_hash(password_hash, password):
+
+        user_id = users.check_login(username, password)
+        if user_id:
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
-        else:
-            return "VIRHE: väärä tunnus tai salasana"
+        return "VIRHE: väärä tunnus tai salasana"
 
 @app.route("/logout")
 def logout():
--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+from werkzeug.security import check_password_hash, generate_password_hash
 import db
 
 def get_users():
@@ -18,3 +19,28 @@ def get_products(user_id):
                 FROM        products
                 WHERE       user_id = ?"""
     return db.query(sql, [user_id])
+
+def check_availability(username):
+    sql = """   SELECT      username
+                FROM        users
+                WHERE       username = ?"""
+    result = db.query(sql, [username])
+    return False if result else True
+
+def create_user(username, password):
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])
+    if not result:
+        return None
+
+    user_id = result[0]["id"]
+    password_hash = result[0]["password_hash"]
+    if check_password_hash(password_hash, password):
+        return user_id
+    else:
+        return None
